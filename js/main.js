de869b87b336fbb8bc712d4de546d04985968d11
nolleto <felipenolletonascimento@gmail.com>
2014-02-27 18:53:17 -0300

    Algumas mudanças .....

--- js/main.js
+++ js/main.js
@@ -1,45 +1,73 @@
-var API_KEY = 'AIzaSyCKmtGaFFMp8vh15tAAm2KzrWNDNiQeZqs';
+
 var iIn = $('#textOriginal');
 var iOut = $('#textTranslated');
 
-function traduzir() {
-	testar(iIn.val());
-}
-
-function concluido(data) {
-	console.log(data);
-}
-
-function testar(texto) {	
-	$.ajax({
-		type: 'GET',
-		url: 'https://www.googleapis.com/language/translate/v2?q=' + texto + '&source=en&target=de&key=' + API_KEY,
-		//data: data,
-		success: function(data) {
-			console.log(data);
-			$('textarea').val(data)
-		}
-		//dataType: dataType
+$( document ).ready(function() {
+	$('button').on('click', function() {
+		var text = iIn.val();
+		var translate = new nTranslate();
+		translate.Translate(text);		
 	});
-}
+	
+});
 
-function detectarIdioma() {
-	$.ajax({
-		type: 'GET',
-		url: 'https://www.googleapis.com/language/translate/v2/detect?q=' + texto + '&key=' + API_KEY,
-		success: function(data) {
-			console.log(data);
+(function() {
+	nTranslate = function() {
+		var API_KEY = 'AIzaSyCKmtGaFFMp8vh15tAAm2KzrWNDNiQeZqs';
+		var self = this;
+
+		self.Translate = function(text, data) {		
+			data = $.extend({
+				q: text,
+				key : API_KEY,
+				source: 'en',
+				target: 'pt'
+			}, data);
+
+			$.ajax({
+				type: 'GET',
+				data: data,
+				url: 'https://www.googleapis.com/language/translate/v2',
+				success: function(data) {
+					console.log(data);
+					var translations = data.data.translations;
+					
+				}
+			});
 		}
-	});
-}
 
-function GetLanguages(){
-	$.ajax({
-		type: 'GET',
-		url: 'https://www.googleapis.com/language/translate/v2/languages?&target=zh-TW&key=' + API_KEY,
-		success: function(data) {
-			console.log(data);
+		self.Detectlanguage = function(text) {
+			$.ajax({
+				type: 'GET',
+				data: { 
+					q: text,
+					key: API_KEY
+				},
+				url: 'https://www.googleapis.com/language/translate/v2/detect',
+				success: function(data) {
+					console.log(data);
+					var detections = data.data.detections;
+				}
+			});
 		}
-	});
 
-}
+		self.GetLanguages = function (){
+			$.ajax({
+				type: 'GET',
+				data: {
+					key: API_KEY
+				},
+				url: 'https://www.googleapis.com/language/translate/v2/languages',
+				success: function(data) {
+					console.log(data);
+					var languages = data.data.languages;
+					
+				}
+			});
+
+		}
+	};
+
+})();
+
+
